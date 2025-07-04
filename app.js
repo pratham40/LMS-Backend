694d0669f9b2b57c98e5b777853f7088f568102e
pratham40 <122166099+pratham40@users.noreply.github.com>
2025-07-04 10:18:15 +0530

    added frontend

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({
 }))
 
 const corsOption={
-    origin:'http://localhost:5173',
+    origin:'https://lms-iota-five.vercel.app/',
     credentials:true,
     methods:['GET','POST','PUT','DELETE','OPTIONS']
 }
