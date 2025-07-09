082edeee489438a3e946fe48ef26e7a83bee6644
pratham40 <122166099+pratham40@users.noreply.github.com>
2025-07-09 10:18:25 +0530

    update CORS origin to allow local development and remove LMS path from database connection URI

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({
 }))
 
 const corsOption={
-    origin:'https://lms-iota-five.vercel.app/',
+    origin:'http://localhost:5173',
     credentials:true,
     methods:['GET','POST','PUT','DELETE','OPTIONS']
 }
--- config/dbConnection.js
+++ config/dbConnection.js
@@ -5,7 +5,7 @@ mongoose.set('strictQuery',false)  // not gave error when extra info is required
 const connectionToDB=async()=>{
     try {
         const {connection} = await mongoose.connect(
-            `${process.env.MONGO_URL}/LMS`,
+            `${process.env.MONGO_URL}`,
         )
         // console.log(connection);
         
